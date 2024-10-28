4e72df16c40117a048b1fc0d50adf070c5fd9a2e
Luke Hennessy <lukehenn@icloud.com>
2024-10-28 11:02:04 -0400

    Revert "Attempts to add duration  and type to note block"
    This reverts commit 3634d3328d97d505b29eb1b63724c10fcad40bf6.

---
+++ examples/getting-started-codelab/complete-code/scripts/sound_blocks.js
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const MIN_OCTAVE = 2; // from double bass, see: https://en.wikipedia.org/wiki/Double_bass#Pitch
+const MAX_OCTAVE = 8; // from double bass, see: https://en.wikipedia.org/wiki/Piccolo
+const NOTES = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const NOTE_VALUES = []; 
+
+
+
+for (let i = MIN_OCTAVE; i <= MAX_OCTAVE; i++) {
+  for (let j = 0; j < NOTES.length; j++) {
+    NOTE_VALUES.push(NOTES[j] + i);
+  }
+}
+
+console.log('NOTES', NOTES);
+console.log('NOTE_VALUES', NOTE_VALUES);
+
+const PLAY_SOUND_OPTIONS = NOTE_VALUES.map((note) => [note, 'sounds/' + note + '.m4a']);
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    type: 'play_sound',
+    message0: 'Play %1',
+    args0: [
+      {
+        type: 'field_dropdown',
+        name: 'VALUE',
+        options: PLAY_SOUND_OPTIONS,
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 355,
+  },
+]);
+
+javascript.javascriptGenerator.forBlock['play_sound'] = function (block) {
+  const value = "'" + block.getFieldValue('VALUE') + "'";
+  return 'MusicMaker.queueSound(' + value + ');\n';
+};
