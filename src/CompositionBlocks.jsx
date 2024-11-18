b43210ccb48b37166e5074c34a7917b8614d6c19
Luke Hennessy <lukehenn@icloud.com>
2024-11-18 13:55:22 -0500

    11/18 Notes

--- src/CompositionBlocks.jsx
+++ src/CompositionBlocks.jsx
@@ -11,13 +11,32 @@ export default function CompositionBlocks({flatJSON}) {
     setToolbox(buildToolBox())
   }, []);
 
+  // change xml to JSON flatio: notes 11/18 
+  // Blockly option:
+  // - Instead of producing python or other code from xml use existing blockly tools to create JSON 
+  //   - expand definition of block to know how to write flatio JSON in each instance of a block
+  
+  // Use straight XML:
+  // - Use a parser to create data from the xml and then convert that data to flatIO JSON
+  
+  
+  // use same the same JSON for everything else except:
+  // each object will have one part with several measures:
+  //   - use measure/group block to collect notes and then for each measure block, replace the 
+  //   existing note array with the updated version
+  // whenever anything is changed, we replace all measures and all notes within measures
 
   return (
     <div className='split-container'>
       {toolbox.contents && <BlocklyWorkspace
         toolboxConfiguration={toolbox} // this must be a JSON toolbox definition
         initialXml={xml}
-        onXmlChange={setXml}
+        // onXmlChange={setXml} 
+        onXmlChange={(newXml) => {
+          console.log("XML Updated:", newXml); // Log the updated XML
+          setXml(newXml); // Update the state
+        }}
+        
         className="fill-height"
         workspaceConfiguration={{
           grid: {
